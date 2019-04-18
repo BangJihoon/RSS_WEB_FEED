bb1e030c8c8a0f32a6b2ff6195971919656614cf
민아 <48783827+jeonmina@users.noreply.github.com>
2019-04-18 15:50:57 +0900

    Create hello.py

---
+++ hello.py
@@ -0,0 +1,17 @@
+from flask import Flask, render_template
+import pymongo
+app = Flask(__name__)
+
+@app.route('/')
+@app.route('/mongo',methods=['POST'])
+def mongoTest():
+    client = pymongo.MongoClient('mongodb+srv://bang:bang@cluster0-uiqcf.mongodb.net/test?retryWrites=true')
+    db = client.get_database('bluevisor')
+    collection = db.get_collection('posts')
+    results = collection.find()
+    client.close()
+    return render_template('tt.html', data=results)
+
+
+if __name__ == '__main__':
+    app.run()
