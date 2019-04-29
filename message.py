7a346b6c6bda20861ca485d1a6a5da819ea388a9
민아 <48783827+jeonmina@users.noreply.github.com>
2019-04-29 15:54:13 +0900

    Update message.py

--- message.py
+++ message.py
@@ -15,25 +15,25 @@ def sendmail():
         contents = file.read()
         # ------------------ 메일보내기 https://yeolco.tistory.com/93 --- 참고
 
-        # 세션생성
-        s = smtplib.SMTP('smtp.gmail.com', 587)
-        # TLS 보안 시작
-        s.starttls()
-
-        # 로그인 인증
-        s.login('jihoon289@gmail.com', 'opjjhvlrvidyfzdo')
-
-        # 보낼 메시지 설정
-        msg = MIMEText(contents)
-        msg['Subject'] = date + ' 공고문 스크랩'
-
-        # 메일 보내기 (보내는사람, 받을사람, 내용)
-        s.sendmail("jihoon289@gmail.com", "jihoon289@naver.com", msg.as_string())
-        s.sendmail("jihoon289@gmail.com", "mina312@naver.com", msg.as_string())
-        s.sendmail("jihoon289@gmail.com", "pakusepgame@gmail.com", msg.as_string())
-
-        # 세션 종료
-        s.quit()
+        # # 세션생성
+        # s = smtplib.SMTP('smtp.gmail.com', 587)
+        # # TLS 보안 시작
+        # s.starttls()
+        #
+        # # 로그인 인증
+        # s.login('jihoon289@gmail.com', 'opjjhvlrvidyfzdo')
+        #
+        # # 보낼 메시지 설정
+        # msg = MIMEText(contents)
+        # msg['Subject'] = date + ' 공고문 스크랩'
+        #
+        # # 메일 보내기 (보내는사람, 받을사람, 내용)
+        # s.sendmail("jihoon289@gmail.com", "jihoon289@naver.com", msg.as_string())
+        # s.sendmail("jihoon289@gmail.com", "mina312@naver.com", msg.as_string())
+        # s.sendmail("jihoon289@gmail.com", "pakusepgame@gmail.com", msg.as_string())
+        #
+        # # 세션 종료
+        # s.quit()
 
 
         # # slack 메세지 보내기
@@ -48,12 +48,15 @@ def sendmail():
         #
         # # slack.chat.post_message("#todays-posts",contents)
 
-    API_KEY = '818222067:AAFJWKcasPwz2DPRZu6HSFmzv8CerHHsm_k'
-    bot = telegram.Bot(token=API_KEY)
+        API_KEY = '818222067:AAFJWKcasPwz2DPRZu6HSFmzv8CerHHsm_k'
+        bot = telegram.Bot(token=API_KEY)
+        #chat_id 확인 -> https://api.telegram.org/bot818222067:AAFJWKcasPwz2DPRZu6HSFmzv8CerHHsm_k/getUpdates
+        chat_id = "-1001360438299"
 
-    chat_id = "-1001360438299"
+        num = mongo.count()
 
-    num = mongo.count()
+        bot.sendMessage(chat_id=chat_id, text="새로운 공고문이 " + str(num) + "개 올라왔습니다. \n자세히 보기 : http://bvpost.shop/")
 
-    bot.sendMessage(chat_id=chat_id, text="새로운 공고문이 " + str(num) + "개 올라왔습니다. \n자세히 보기 : http://bvpost.shop/")
+        chat_id = "-1001447356505"
 
+        bot.sendMessage(chat_id=chat_id, text= contents)
