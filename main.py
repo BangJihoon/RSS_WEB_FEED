a345d684c36d5a099eeaf50eff1adbe9efc7a51c
jihoon289 <jihoon289@naver.com>
2019-05-02 16:48:13 +0900

    더 많은 사이트 추가

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
 from selenium import webdriver
-from datetime import datetime, timedelta
+from datetime import datetime
 from selenium.webdriver.chrome.options import Options
 import sites
 import mongo, message
@@ -20,18 +20,35 @@ sys.stdout = open('result.txt', 'w',encoding='UTF8')
 date = datetime.now().strftime('%Y-%m-%d %H:%M')
 print(date + ' 실행결과\n\n')
 
+# ---------------------------- 동적인 웹 -----------------------
+'''
+    msit, 
+    kstartup
+    seoul
+    venture
+    ccei
+'''
+
 # 과학기술정보통신부
 sites.msit_scan(driver)
 
 # K-스타트업 (창업넷)
 sites.kstartup_scan(driver)
 
+# 서울특별시
+sites.seoul_scan(driver)
+
+# 벤처기업협회
+sites.venture_scan(driver)
+
+# 벤처기업협회
+sites.ccei_scan(driver)
+
 # 창닫기
 driver.close()
 
-''' 
-kotra, nipa, sba, kisa, nia, kdata, moel, bepa
-'''
+# ----------------- 정적인 웹 크롤링 ---------------------------
+
 
 # 코트라
 sites.kotra_scan()
@@ -65,6 +82,39 @@ sites.kised_scan()
 
 # 부산정보산업진흥원
 sites.busanit_scan()
+
+# 산업통상자원부
+sites.motie_scan()
+
+# 서울창업허브
+sites.seoulstartuphub_scan()
+
+# 한국산업진흥협회
+sites.koita_scan()
+
+# 정보통신정책연구원
+sites.kisdi_scan()
+
+# 지능정보산업협회
+sites.kai_scan()
+
+# 부산광역시
+sites.busan_scan()
+
+# 한국인공지능협회
+sites.koraia_scan()
+
+# 아이디어마루
+sites.ideamaru_scan()
+
+# 부산테크노파크_사업공고
+sites.btp_scan()
+
+# 서울기업지원센터
+sites.sbsc_scan()
+
+# 중소벤처기업부
+sites.mss_scan()
 
 sys.stdout.close()
 
